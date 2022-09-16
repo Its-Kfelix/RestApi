efe510ffc35ea1eac1cc056bb0feac40e56cfbf0
its-kfelix <felixodhiambo538@gmail.com>
2022-09-16 14:23:30 +0300

    Implemented both get a singe item and get all methods rest methods

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rpg3.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController: ControllerBase
+{
+    private static List<Character> characters = new List<Character>()
+    {
+        new Character { Id = 0, Name = "David" },
+        new Character { Id = 1, Name = "Solomon" },
+        new Character { Id = 2, Name = "John" },
+        new Character { Id = 3, Name = "Ian" },
+        new Character { Id = 4, Name = "Alex" },
+        new Character { Id = 5, Name = "Joseph" }
+    };
+    [HttpGet("GetAll")]
+    public ActionResult<List<Character>> Get()
+    {
+        return Ok(characters);
+    }
+    [HttpGet]
+    public ActionResult<Character> GetSingleCharacter(int id)
+    {
+        return Ok(characters.FirstOrDefault(c=> c.Id == id));
+    }
+}
+
---
+++ Models/RpgClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Rpg3.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum RpgClass
+{
+    Knight = 1,
+    
+    Mage = 2,
+    
+    Cleric = 3
+}
